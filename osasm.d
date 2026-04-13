ca076efeef5272ac5b9ca687a50979a10075babf
LukeNguyen1234 <156454492+LukeNguyen1234@users.noreply.github.com>
2026-04-13 09:43:20 -0500

    Add files via upload

---
+++ osasm.d
@@ -0,0 +1,35 @@
+Dependencies for Project 'Lab2', Target 'Target': (DO NOT MODIFY !)
+CompilerVersion: 5060960::V5.06 update 7 (build 960)::.\ARM_Compiler_5.06u7
+F (.\Lab2.c)(0x69C47B06)(--c99 -c --cpu Cortex-M4 -D__EVAL -g -O0 --apcs=interwork --split_sections --signed_chars -I ../inc-I.\RTE\_Target-I"D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include"-I"D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123"-D__UVISION_VERSION="543" -DTM4C123GH6PM -D_RTE_-o .\objects\lab2.o --omf_browse .\objects\lab2.crf --depend .\objects\lab2.d)
+I (D:\ARM Files\Kiel_V5\ARM\ARM_Compiler_5.06u7\include\stdint.h)(0x5E8F01A2)
+I (../inc/BSP.h)(0x5C0AD050)
+I (../inc/Profile.h)(0x57E526D6)
+I (Texas.h)(0x56D8C78C)
+I (../inc/CortexM.h)(0x57E52658)
+I (os.h)(0x69C47B14)
+F (.\osasm.s)(0x69C47377)(--cpu Cortex-M4 --pd "__EVAL SETA 1" -g --apcs=interwork -I.\RTE\_Target-I"D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include"-I"D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123"--pd "__UVISION_VERSION SETA 543" --pd "TM4C123GH6PM SETA 1" --pd "_RTE_ SETA 1"--list .\listings\osasm.lst --xref -o .\objects\osasm.o --depend .\objects\osasm.d)
+F (.\os.c)(0x69C47377)(--c99 -c --cpu Cortex-M4 -D__EVAL -g -O0 --apcs=interwork --split_sections --signed_chars -I ../inc-I.\RTE\_Target-I"D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include"-I"D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123"-D__UVISION_VERSION="543" -DTM4C123GH6PM -D_RTE_-o .\objects\os.o --omf_browse .\objects\os.crf --depend .\objects\os.d)
+I (D:\ARM Files\Kiel_V5\ARM\ARM_Compiler_5.06u7\include\stdint.h)(0x5E8F01A2)
+I (os.h)(0x69C47B14)
+I (../inc/CortexM.h)(0x57E52658)
+I (../inc/BSP.h)(0x5C0AD050)
+F (..\inc\BSP.c)(0x69863BCC)(--c99 -c --cpu Cortex-M4 -D__EVAL -g -O0 --apcs=interwork --split_sections --signed_chars -I ../inc-I.\RTE\_Target-I"D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include"-I"D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123"-D__UVISION_VERSION="543" -DTM4C123GH6PM -D_RTE_-o .\objects\bsp.o --omf_browse .\objects\bsp.crf --depend .\objects\bsp.d)
+I (D:\ARM Files\Kiel_V5\ARM\ARM_Compiler_5.06u7\include\stdint.h)(0x5E8F01A2)
+I (..\inc\BSP.h)(0x5C0AD050)
+I (..\inc\../inc/tm4c123gh6pm.h)(0x5388C2DA)
+F (..\inc\Profile.c)(0x6987F179)(--c99 -c --cpu Cortex-M4 -D__EVAL -g -O0 --apcs=interwork --split_sections --signed_chars -I ../inc-I.\RTE\_Target-I"D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include"-I"D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123"-D__UVISION_VERSION="543" -DTM4C123GH6PM -D_RTE_-o .\objects\profile.o --omf_browse .\objects\profile.crf --depend .\objects\profile.d)
+I (D:\ARM Files\Kiel_V5\ARM\ARM_Compiler_5.06u7\include\stdint.h)(0x5E8F01A2)
+I (..\inc\../inc/tm4c123gh6pm.h)(0x5388C2DA)
+I (..\inc\BSP.h)(0x5C0AD050)
+F (.\texas.o)(0x57C78E76)()
+F (RTE/Device/TM4C123GH6PM/startup_TM4C123.s)(0x56BBE4A6)(--cpu Cortex-M4 --pd "__EVAL SETA 1" -g --apcs=interwork -I.\RTE\_Target-I"D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include"-I"D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123"--pd "__UVISION_VERSION SETA 543" --pd "TM4C123GH6PM SETA 1" --pd "_RTE_ SETA 1"--list .\listings\startup_tm4c123.lst --xref -o .\objects\startup_tm4c123.o --depend .\objects\startup_tm4c123.d)
+F (RTE/Device/TM4C123GH6PM/system_TM4C123.c)(0x52540E94)(--c99 -c --cpu Cortex-M4 -D__EVAL -g -O0 --apcs=interwork --split_sections --signed_chars -I ../inc-I.\RTE\_Target-I"D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include"-I"D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123"-D__UVISION_VERSION="543" -DTM4C123GH6PM -D_RTE_-o .\objects\system_tm4c123.o --omf_browse .\objects\system_tm4c123.crf --depend .\objects\system_tm4c123.d)
+I (D:\ARM Files\Kiel_V5\ARM\ARM_Compiler_5.06u7\include\stdint.h)(0x5E8F01A2)
+I (D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h)(0x6982E370)
+I (D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h)(0x6982E370)
+I (D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h)(0x6982E3D0)
+I (D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h)(0x6982E3D0)
+I (D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h)(0x6982E3D0)
+I (D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h)(0x6982E3D0)
+I (D:\ARM Files\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h)(0x6982E3D0)
+I (D:\ARM Files\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h)(0x6982E36F)
